7d5babdd424437b4f69380626ba27eeffa71c8b4
Oluwatimilehin Idowu <oluwatimilehin.id@gmail.com>
2024-07-01 07:27:08 +0100

    Update settings.py

--- web-server/web_server/web_server/settings.py
+++ web-server/web_server/web_server/settings.py
@@ -13,12 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 import environ
-from dotenv import load_dotenv
 
 load_dotenv()
-
-env = environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
