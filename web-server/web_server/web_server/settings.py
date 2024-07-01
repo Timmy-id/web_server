08e0866e02891d28608c17c097219f2af8d61171
Oluwatimilehin Idowu <oluwatimilehin.id@gmail.com>
2024-07-01 10:28:51 +0100

    Update settings.py

--- web-server/web_server/web_server/settings.py
+++ web-server/web_server/web_server/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
-import environ
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
