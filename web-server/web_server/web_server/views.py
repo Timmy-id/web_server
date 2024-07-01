756a07048de0bc273cf535dfb45a4b8356f97a6b
Timmy9513 <157916744+Timmy9513@users.noreply.github.com>
2024-07-01 21:38:39 +0100

    Refactor the views file

--- web-server/web_server/web_server/views.py
+++ web-server/web_server/web_server/views.py
@@ -10,9 +10,14 @@ load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
 
-def hello(request):
-    ip = geocoder.ip("me").json
+ip = geocoder.ip("me").json
+
+def getUrlData():
     data = requests.get(f'http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={ip}').json()
+    return data
+
+def hello(request):
+    data = getUrlData()
     visitor_name = request.GET.get("visitor_name")
     temperature = data["current"]["temp_c"]
     location = data["location"]["name"]
