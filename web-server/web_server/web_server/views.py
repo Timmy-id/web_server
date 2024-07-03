f182c8c79285032cde48b16ae92f35255e9df85f
Timmy9513 <157916744+Timmy9513@users.noreply.github.com>
2024-07-03 21:45:22 +0100

    Refactor the views file

--- web-server/web_server/web_server/views.py
+++ web-server/web_server/web_server/views.py
@@ -6,28 +6,29 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
-API_KEY = os.getenv("API_KEY")
 IPINFO_TOKEN = os.getenv("IPINFO_TOKEN")
+OPENAPI_TOKEN = os.getenv("OPENAPI_TOKEN")
 
 handler = ipinfo.getHandler(IPINFO_TOKEN)
 details = handler.getDetails().details
 
 ip = details["ip"]
+lat = details["latitude"]
+lon = details["longitude"]
 
 def getUrlData():
-    data = requests.get(f'http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={ip}').json()
+    data = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={OPENAPI_TOKEN}&units=metric').json()
     return data
 
 def hello(request):
-    my_ip = request.META["REMOTE_ADDR"]
     data = getUrlData()
+    # my_ip = request.META["REMOTE_ADDR"]
     visitor_name = request.GET.get("visitor_name")
-    temperature = data["current"]["temp_c"]
-    location = data["location"]["name"]
+    temperature = data["main"]["temp"]
+    location = data["name"]
 
     response = {
-        "client_ip": my_ip,
+        "client_ip": ip,
         "location": location,
         "greeting": f"Hello, {visitor_name}!, the temperature is {temperature} degrees Celcius in {location}",
     }
